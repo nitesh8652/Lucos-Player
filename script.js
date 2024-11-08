462ea4a9ea64704e65134f583033f1b12db301d4
Nitesh Salian <niteshsalian8652085233@gmail.com>
2024-11-08 21:45:57 +0530

    addind cards

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ async function fetchingsngs() {
     for (let index = 0; index < mp.length; index++) {
         const element = mp[index];
         if (element.href.includes(".mp3")) {
-            mp3.push(element.href)
+            mp3.push(element.href.split("/songs/")[1])
         }
     }
     return mp3
@@ -21,8 +21,18 @@ async function main(){
     let mp3 = await fetchingsngs();
     console.log(mp3);
 
+let songlist=document.querySelector(".que").getElementsByTagName("ul")[0];
+for (const song of mp3) {
+    songlist.innerHTML = songlist.innerHTML + `<li> ${song.replaceAll("%20", " ")} </li>`;
+}
     let audio = new Audio(mp3[0]);
     audio.play();
+    
+    audio.addEventListener("loadeddata",()=>{
+        let duration = audio.duration;
+        console.log(duration);
+    })
+
 }
 
 main();
